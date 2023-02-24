1aa4bad9cf3c51e37547ea685e1ba989552e9f9e
JNasri <91850229+JNasri@users.noreply.github.com>
2023-02-24 20:09:23 +0300

    Lists & Keys

--- src/Content.js
+++ src/Content.js
@@ -7,43 +7,90 @@ import React from "react";
 
 // importing useState hook
 import { useState } from "react";
+// importing fontAwesome trash icon
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
   // below is a code for the useState hook
   // name is the name to be shown and setName changes the name
-  // Joe is default name when we render the page for the first time
-  const [name, setName] = useState("Joe");
 
-  // another useState hook example
-  const [count, setCount] = useState(0);
+  // useState(/*here is the default array of books*/)
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Book #8392 : The Diary Of Jane    ",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Book #981 : The Compound Effect    ",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Book #2937 : Atomic Habits    ",
+    },
+  ]);
 
-  const handleNames = () => {
-    const names = ["Mike", "Rob", "Jon"];
-    const int = Math.floor(Math.random() * 3);
-    // call the useState function to change the name
-    setName(names[int]);
+  const handleCheck = (id) => {
+    //console.log(`key : ${id}`);
+    // we will use map() to create a new array that specifically changes the checked for the ID passed
+    // if found (?) we retrun the item object with flipped checked (true -> false or vise versa)
+    // if not found (:) we return the item object with no changes
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    // now after we updated the array, we will set the items using the useState hook
+    setItems(listItems);
+    // localstorage to save the changes even after reloading the page (not much important)
+    localStorage.setItem("ShoppingList", JSON.stringify(listItems));
   };
 
-  const handleClick = () => {
-    console.log(count);
+  const handleDelete = (id) => {
+    // console.log(`key : ${id}`);
+    // filter() creates a new array without the item  that has the ID passed to the function
+    const listItems = items.filter((item) => item.id !== id);
+    // now after we deleted the item, we will set the new array using the useState hook
+    setItems(listItems);
+    // localstorage to save the changes even after reloading the page (not much important)
+    localStorage.setItem("ShoppingList", JSON.stringify(listItems));
   };
 
-  const handleClick2 = (name) => {
-    console.log(`Hello, ${name}`);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e);
-  };
   return (
     <main>
-      <p>Hello {name}</p>
-      <button onClick={handleNames}>Change Name</button>
-      <button onClick={() => setCount(count + 1)}>Count ++</button>
-      <button onClick={handleClick}>Check Count</button>
-
-      <button onClick={() => handleClick2("Dave")}>Click Me</button>
-      <button onClick={(e) => handleClick3(e)}>Click Me</button>
+      {/* check if the length is > 0 , if not present a message written down */}
+      {items.length ? (
+        // {/* unordered list to display books */}
+        <ul>
+          {/* map function to map the array of books  */}
+          {items.map((item) => (
+            // we see below that a list element needs a key so that react can keep track of each item. the key can be the id (unique)
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleCheck(item.id)}
+              />
+              <label
+                onDoubleClick={() => handleCheck(item.id)}
+                style={item.checked ? { textDecoration: "line-through" } : null}
+              >
+                {item.item}
+              </label>
+              {/* here we added a react icon instead of a button */}
+              <FaTrashAlt
+                role="button"
+                tabIndex="0"
+                cursor="pointer"
+                onClick={() => handleDelete(item.id)}
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p styel={{ marginTop: "2rem" }}>List is Empty</p>
+      )}
     </main>
   );
 };
